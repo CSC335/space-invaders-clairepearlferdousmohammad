fbe95c04297a5900c6ba676df3d3bd23d99a490e
Pearl McNabb <pearlmcnabb@Pearls-MacBook-Pro.local>
2024-04-16 16:37:56 -0700

    JavaDocs for PlayerTank and Bullet classes

--- src/model/Bullet.java
+++ src/model/Bullet.java
@@ -1,8 +1,13 @@
-// @author: Frances Pearl McNabb
 package model;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+
+/**
+ * Represents an instance of a bullet within a Space Invaders game.
+ * 
+ * @author Pearl McNabb
+ */
 
 public class Bullet {
 
@@ -18,49 +23,81 @@ public class Bullet {
 		this.alienShoot = alienFire;
 	}
 
+	/**
+	 * Gets the x-coordinate position of the bullet
+	 * 
+	 * @return xPosition The current x position of the bullet
+	 */
 	public float getX() {
 		return this.xPosition;
 	}
 
+	/**
+	 * Gets the y-coordinate position of the bullet
+	 * 
+	 * @return yPosition The current y position of the bullet
+	 */
 	public float getY() {
 		return this.yPosition;
 	}
 
+	/**
+	 * Decreases the y-coordinate position of the bullet by 1 Moves the bullet up in
+	 * terms of the canvas coordinate system
+	 */
 	public void moveUp() {
 		this.yPosition -= 1.0;
 	}
 
+	/**
+	 * Increases the y-coordinate position of the bullet by 1 Moves the bullet up in
+	 * terms of the canvas coordinate system
+	 */
 	public void moveDown() {
 		this.yPosition += 1.0;
 	}
 
+	/**
+	 * Gets the image to put on the canvas to represent the bullet
+	 * 
+	 * @return image The image that should be shown to represent the bullet
+	 */
 	public Image getImage() {
 		return this.image;
 	}
-	
+
+	/**
+	 * Draws the bullet using the given image onto the graphics context
+	 * 
+	 * @param gc GraphicsContext gc that is used in the GUI
+	 */
 	public void draw(GraphicsContext gc) {
 		gc.drawImage(image, xPosition, yPosition);
 	}
-	
+
+	/**
+	 * If an alien is firing down, the bullet moves down. If the tank is firing, the
+	 * bullet moves up. Both with sleep to make the bullet seem as though it is
+	 * moving and both move until hitting the edge of the canvas.
+	 */
 	public void fire() {
 		if (!this.alienShoot) {
 			while (this.yPosition >= 1) {
 				try {
 					// if waiting 2 milliseconds, it will take 1200 seconds to go across the screen
-					  Thread.sleep(1);
-					} catch (InterruptedException e) {
-					  Thread.currentThread().interrupt();
-					}
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+				}
 				moveUp();
 			}
-		}
-		else {
+		} else {
 			while (this.yPosition <= 599.0) {
 				try {
-					  Thread.sleep(1);
-					} catch (InterruptedException e) {
-					  Thread.currentThread().interrupt();
-					}
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+				}
 				moveDown();
 			}
 		}
--- src/model/PlayerTank.java
+++ src/model/PlayerTank.java
@@ -1,8 +1,14 @@
-// @author: Frances Pearl McNabb
 package model;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+
+/**
+ * Represents a playerTank, movable by the player, in an instance of a Space
+ * Invaders game.
+ * 
+ * @author Pearl McNabb
+ */
 
 public class PlayerTank {
 
@@ -18,34 +24,73 @@ public class PlayerTank {
 		this.image = new Image(getClass().getResourceAsStream("tank.png"));
 	}
 
+	/**
+	 * Gets the x-coordinate position of the tank
+	 * 
+	 * @return xPosition The current x position of the tank
+	 */
 	public int getX() {
 		return this.xPosition;
 	}
 
+	/**
+	 * Gets the y-coordinate position of the tank
+	 * 
+	 * @return yPosition The current y position of the tank
+	 */
 	public int getY() {
 		return this.yPosition;
 	}
 
+	/**
+	 * Decreases the x-coordinate position of the tank by 1 Moves the tank left in
+	 * terms of the canvas coordinate system
+	 */
 	public void moveLeft() {
 		this.xPosition -= 1;
 	}
 
+	/**
+	 * Increases the x-coordinate position of the tank by 1 Moves the tank right in
+	 * terms of the canvas coordinate system
+	 */
 	public void moveRight() {
 		this.xPosition += 1;
 	}
 
+	/**
+	 * Gets the amount of damage that the tank has taken during the game
+	 * 
+	 * @return damages The current damage on the tank
+	 */
 	public int getDamages() {
 		return damages;
 	}
 
+	/**
+	 * Gets the image to put on the canvas to represent the tank
+	 * 
+	 * @return image The image that should be shown to represent the tank
+	 */
 	public Image getImage() {
 		return image;
 	}
 
+	/**
+	 * When the tank takes on damages, it must accumulate this amount into damages
+	 * to keep track of how much damages until death
+	 * 
+	 * @param damages Amount of damages that the tank took
+	 */
 	public void addDamages(int damages) {
 		this.damages += damages;
 	}
-	
+
+	/**
+	 * Draws the tank using the given image onto the graphics context
+	 * 
+	 * @param gc GraphicsContext gc that is used in the GUI
+	 */
 	public void draw(GraphicsContext gc) {
 		gc.drawImage(image, xPosition, yPosition);
 	}
