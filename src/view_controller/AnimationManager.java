80cbc9f6e9a374f576a534645890ca054c78b8e4
Pearl McNabb <pearlmcnabb@dhcp-10-134-214-136.uawifi.arizona.edu>
2024-04-17 15:08:22 -0700

    added animationmanager class

---
+++ src/view_controller/AnimationManager.java
@@ -0,0 +1,18 @@
+package view_controller;
+
+public class AnimationManager {
+	
+	public AnimationManager(boolean tankDeath, boolean alienDeath, boolean extra, double xPos, double yPos) {
+		
+	}
+	
+	public void tankDying() {
+		
+	}
+	
+	public void alienDying() {
+		
+	}
+	
+	//public void extra() {}
+}
